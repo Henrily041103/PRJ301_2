8d436d9d7af2c6111ff5062688d6e55fd98b484a
Wave <ngnphucan@gmail.com>
2023-03-26 17:21:34 +0700

    receipt

--- PRJ301/src/java/controller/CartController.java
+++ PRJ301/src/java/controller/CartController.java
@@ -89,7 +89,7 @@ public class CartController extends HttpServlet {
                             break;
                         case "buy":
                             buy(request, response, cart, session, pdao, odao);
-                            response.sendRedirect(request.getContextPath() + "/" + CART_CONTROLLER + "/" + RECEIPT + ".do");
+
                             break;
                         case "add":
                             changeAmount(request, response, cart, session, pdao, 1);
@@ -102,9 +102,6 @@ public class CartController extends HttpServlet {
                         default:
                             throw new ServletException();
                     }
-                case "receipt":
-                    
-                    break;
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -163,10 +160,10 @@ public class CartController extends HttpServlet {
             request.setAttribute("orders", orders);
             request.setAttribute("controller", CART_CONTROLLER);
             request.setAttribute("action", RECEIPT);
-        }
-        else {
-            request.setAttribute("controller", CART_CONTROLLER);
-            request.setAttribute("action", RECEIPT);
+            request.getRequestDispatcher(MAIN).forward(request, response);
+        } else {
+            request.setAttribute("controller", SHOP_CONTROLLER);
+            request.setAttribute("action", SHOP);
         }
     }
 
